67d95ed776c676c91584f6c91caf5c5129f370d5
useop <useop0311@gmail.com>
2025-08-14 01:28:33 +0900

    fix spawn zone command import

--- src/main/kotlin/org/useop0311/ryoTenKa/SpawnZoneCommand.kt
+++ src/main/kotlin/org/useop0311/ryoTenKa/SpawnZoneCommand.kt
@@ -12,6 +12,7 @@ import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitRunnable
 import org.useop0311.ryoTenKa.RyoTenKa.Companion.deathCounter
 import org.useop0311.ryoTenKa.RyoTenKa.Companion.doklib
 import java.util.TimerTask
