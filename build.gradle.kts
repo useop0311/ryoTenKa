8a3e02b70fe55c971ce214966ff7d37a6797e04b
useop <61257727+useop0311@users.noreply.github.com>
2025-08-16 00:49:14 +0900

    fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.github.useop0311:ryoTenKa:2a1daaa851")
 }
 
 tasks {
