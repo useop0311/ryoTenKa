e47f4f42f471293ac5af37f554be7469a2e3acbc
useop <61257727+useop0311@users.noreply.github.com>
2025-08-16 00:13:04 +0900

    delete build path

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.github.useop0311:ryoTenKa:Tag")
+    implementation("c
 }
 
 tasks {
@@ -56,15 +56,4 @@ tasks.withType<ShadowJar> {
     // 1. 파일 이름 관련 설정
     archiveBaseName.set("ryoTenKa")
     archiveClassifier.set(null as String?)          // 접미사 없음
-
-    // 2. 파일 위치 관련 설정
-    destinationDirectory.set(file("/Users/useop0311/Development/minecraft/servers/pltest-paper-218/plugins/")) // 특정 절대 경로에 바로 저장
-}
-
-dependencyResolutionManagement {
-	repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-	repositories {
-		mavenCentral()
-		maven { url = uri("https://jitpack.io") }
-	}
 }
